c37920b8e28bcad6d4eb89be461e34bc6a31b8d9
Henry Álvarez Astudillo <halvarezast@gmail.com>
2026-02-27 09:56:42 -0500

    Update956

--- src/config/global.js
+++ src/config/global.js
@@ -105,7 +105,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/123101_CF05_DU.zip',
+        download: 'downloads/123103_CF05_DU.zip',
       },
       {
         icono: 'fas fa-download',
